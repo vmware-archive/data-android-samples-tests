967f8a20303a035838962379e756f5e5c54e7c0e
Devin Fallak and Josh Winters <jwinters@pivotal.io>
2015-06-18 12:37:38 -0400

    Adds pivotal.auth.scopes to properties

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -9,6 +9,7 @@ cat > ./data-demo/src/main/assets/pivotal.properties << EOM
 pivotal.auth.tokenUrl=$DATA_ACCEPTANCE_AUTH_URL/token
 pivotal.auth.clientId=$DATA_ACCEPTANCE_CLIENT_ID
 pivotal.auth.clientSecret=$DATA_ACCEPTANCE_CLIENT_SECRET
+pivotal.auth.scopes=openid offline_access
 
 pivotal.auth.accountType=io.pivotal.android.demo.account
 pivotal.auth.tokenType=io.pivotal.android.demo.token
